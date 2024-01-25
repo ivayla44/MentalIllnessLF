e3396d2616a28625d5b1282d15e5f5e76bb4b76a
iva <ivayla@baruh.net>
2024-01-25 23:23:07 +0200

    db err fix

--- mentillfr/src/pages/index.js
+++ mentillfr/src/pages/index.js
@@ -25,49 +25,68 @@ export default function Home({myPosts, friendsPosts}) {
         <div>{createPost()}</div>
 
         <h2>View my posts</h2>
-        <div>{
-          myPosts.map((post) => (
-              <div key={post.id}>
-                <div>{post.title}</div>
-                <div>{post.content}</div>
-              </div>
-          ))
-        }</div>
+          <div>
+              {myPosts !== null ? (
+                  myPosts.map((post) => (
+                      <div key={post.id}>
+                          <div>{post.title}</div>
+                          <div>{post.content}</div>
+                      </div>
+                  ))
+              ) : (
+                  <p>No posts available.</p>
+              )}
+          </div>
 
         <h2>View feed</h2>
-        <div>{
-          friendsPosts.map((post) => (
-              <div key={post.id}>
-                <div>{post.title}</div>
-                <div>{post.content}</div>
-              </div>
-          ))
-        }</div>
+          <div>
+              {friendsPosts !== null ? (
+                  friendsPosts.map((post) => (
+                      <div key={post.id}>
+                          <div>{post.title}</div>
+                          <div>{post.content}</div>
+                      </div>
+                  ))
+              ) : (
+                  <p>No feed posts available.</p>
+              )}
+          </div>
       </div>
   )
 }
 
 export async function getServerSideProps(context) {
-  // TODO: shte vidim kak shte vzimame id-tata, ili nqkakuv session/cookie-ta not sure yet
-  const currentUserID = 1;
+    // TODO: shte vidim kak shte vzimame id-tata, ig nqkakuv session/cookie-ta not sure yet
+    const currentUserID = 1;
 
-  const myPosts = await db.get(`
+    try {
+        const myPosts = await db.get(`
         SELECT *
         FROM posts
         WHERE user_id = ?;
     `, [currentUserID]);
 
-  const friendsPosts = await db.get(`
+        const friendsPosts = await db.get(`
         SELECT posts.*
         FROM posts
         INNER JOIN friendships ON (friendships.user_id = posts.user_id OR friendships.friend_id = posts.user_id)
         WHERE friendships.user_id = ? OR friendships.friend_id = ?;
     `, [currentUserID, currentUserID]);
 
-  return {
-    props: {
-      myPosts,
-      friendsPosts
-    },
-  };
+        return {
+            props: {
+                myPosts: myPosts || null,
+                friendsPosts: friendsPosts || null,
+            },
+        };
+    } catch (error) {
+        console.error("Error fetching data:", error);
+
+        return {
+            props: {
+                myPosts: null,
+                friendsPosts: null,
+            },
+        };
+    }
 }
